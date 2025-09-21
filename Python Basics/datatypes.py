197ab8f0df624c9e522530e291893f11b1518dea
K M Marziul Karim <marziulrafi@gmail.com>
2025-09-21 16:11:35 +0600

    setup python

---
+++ Python Basics/datatypes.py
@@ -0,0 +1,17 @@
+age = 12
+name = "karim"
+is_single = True
+interest = 12.50
+
+print(age, type(age))
+print(name, type(name))
+print(is_single, type(is_single))
+print(interest, type(interest))
+
+# Single Line comment
+
+""" 
+Multi-Line Comment
+Press Alt + Shift + A
+
+"""
---
+++ Python Basics/input.py
@@ -0,0 +1,15 @@
+first_name = input("Enter first name : ")
+last_name = input("Enter last name : ")
+print(first_name + last_name)
+
+
+first_money = input("Enter first amount : ")
+second_money = input("Enter second amount : ")
+print(first_money + second_money)
+
+#After typecasting
+
+first_money = input("Enter first amount : ")
+second_money = input("Enter second amount : ")
+print(int(first_money) + int(second_money))
+
